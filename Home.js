3409494a1d0e438607877f0890460c744b7ebb10
kunaljirage <86543735+kunaljirage@users.noreply.github.com>
2022-01-30 19:23:35 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Kunal from './kunal'
+
+
+export default function Home() {
+    return (
+     <>
+     <NavBar/>     
+   <Kunal/>
+        </>
+    )
+}
